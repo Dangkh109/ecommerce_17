50e6130c163e3f12c87821db0e2dd361dcff7f54
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2017-02-22 11:52:49 +0700

    first commit show hot

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @products = Array.new
+    @products.push Product.select{|prod|
+      prod.is_hot}.shuffle.take(Settings.Product_take_home)
+    @products.push Product.order("RANDOM()").take(Settings.Product_take_home)
   end
 
   def about
